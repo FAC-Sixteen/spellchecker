390eb2d76c4ec68393d22a6bcf5c03e6756eca98
Freddie <freddiepreece@outlook.com>
2019-03-27 16:57:15 +0000

    Added images and readFile func

--- src/scripts/handler.js
+++ src/scripts/handler.js
@@ -1,22 +1,12 @@
-const fs = require("fs");
-const path = require("path");
+const readFile = require("./readFile.js");
 
 const handler = (req, res) => {
     const endpoint = req.url;
-    if (endpoint === "/") {
-        fs.readFile(path.join(__dirname, "..", "..", "public", "index.html"), (error, file) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            res.writeHead(200, { "Content-Type" : "text/html" });
-            res.end(file);
-        });
-    }
-    else {
-        res.writeHead(404, { "Content-Type" : "text/plain" });
-        res.end("This page cannot be found in the Room of Requirement!");
-    }
+    readFile(endpoint, res);
+    // if {
+    //     res.writeHead(404, { "Content-Type" : "text/plain" });
+    //     res.end("This page cannot be found in the Room of Requirement!");
+    // }
 }
 
 module.exports = handler;
---
+++ src/scripts/readFile.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+const readFile = (variable, res) => {
+
+    let placeholder = path.basename(variable); 
+    if (variable == '/') {
+        placeholder = "index";
+    }
+    if (!path.extname(variable)) {
+        placeholder += ".html";
+    }
+
+    const object = { 
+        ".html" : "text/html",
+        ".css" : "text/css",
+        ".js" : "text/javascript",
+        ".png" : "image/png",
+        ".jpg" : "image/jpeg",
+        ".jpeg" : "image/jpeg"
+       }
+
+    const prop = path.extname(placeholder);
+
+    fs.readFile(path.join(__dirname, "..", "..", "public", placeholder), "utf8", (err, file) => {
+        if (err) {
+        res.writeHead(500, {'content-type': 'text/plain'});
+        res.end('server error');
+        console.log(err);
+    } else {
+        res.writeHead(200, {"content-type": object[prop]});
+        console.log(placeholder);
+        console.log(path.join(__dirname, "..", "..", "public", placeholder));
+        console.log(object[prop]);
+        res.end(file);
+    }
+    });
+}
+
+module.exports = readFile;
