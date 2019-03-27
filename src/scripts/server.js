6c43f057b12cd263b22da237a3c76c4bd65c1d54
Joko Sanyang <jo.san@live.co.uk>
2019-03-27 14:21:46 +0000

    open server

---
+++ src/scripts/server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const server = http.createServer(handler);
+const port = 1989;
+
+server.listen(port);
+
+console.log(`We have apparated onto port ${port}!`);
+
+module.exports = server;
